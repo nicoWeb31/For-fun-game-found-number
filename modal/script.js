7aed349bcdf46e8750cdd9c11c8fa4305fd22e44
nicoWeb31 <nico.riot@free.fr>
2020-11-28 19:05:17 +0100

    modal refacto

--- modal/script.js
+++ modal/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
 
+const hiddenModal = () =>{
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const openModal = () =>{
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -8,15 +18,17 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log("🚀 ~ file: script.js ~ line 8 ~ btnOpenModal", btnOpenModal)
 
-for(let i = 0; i < btnOpenModal.length; i++) 
-btnOpenModal[i].addEventListener('click', function(e){
-
+for(let i = 0; i < btnOpenModal.length; i++){
     console.log(btnOpenModal[i].textContent, 'click')
-    modal.classList.remove('hidden')
+    
+    btnOpenModal[i].addEventListener('click', openModal)
+    btnCloseModal.addEventListener('click',hiddenModal)
+    overlay.addEventListener('click',hiddenModal)
+    
+}
 
-    btnCloseModal.addEventListener('click', function(e){
-        modal.classList.add('hidden')
-    })
-})
+
+
+
 
 
